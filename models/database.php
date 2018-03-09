19da71a00e228f2397f9b6d11edfc3d351c2c252
Ashton Knox <aknox3@mail.greenriver.edu>
2018-03-09 17:25:53 -0500

    added code to create clients table to database file

--- models/database.php
+++ models/database.php
@@ -1,13 +1,23 @@
 <?php
 /*
   CREATE TABLE projects (
-    id int PRIMARY KEY AUTO_INCREMENT,
+    projectID int PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(40),
     description VARCHAR(300),
     clientCompany VARCHAR(50),
     clientLocation VARCHAR(50),
-    clientSite VARCHAR(100)
+    clientSite VARCHAR(100),
+    status VARCHAR(30)
     );
  *
- *
+create table clients (
+    clientID int PRIMARY KEY AUTO_INCREMENT,
+    clientName VARCHAR(50),
+    clientEmail VARCHAR(50),
+    clientPhone VARCHAR(50),
+    projectID int,
+    FOREIGN KEY(projectID) REFERENCES projects(projectID)
+    );
+   *
+
  */
