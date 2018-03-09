3047d1e8000e29d13e9ad0c7d73e5fcae7df33d6
Ashton Knox <aknox3@mail.greenriver.edu>
2018-03-09 17:16:21 -0500

    Created database file with code to create projects table

---
+++ models/database.php
@@ -0,0 +1,13 @@
+<?php
+/*
+  CREATE TABLE projects (
+    id int PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(40),
+    description VARCHAR(300),
+    clientCompany VARCHAR(50),
+    clientLocation VARCHAR(50),
+    clientSite VARCHAR(100)
+    );
+ *
+ *
+ */
