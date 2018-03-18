99a0ba3ff74015c8c6b8279699bc80d7471adb74
Mathias Taylor <mtaylor53@mail.greenriver.edu>
2018-03-18 17:45:05 -0400

    Fixed merge

--- index.php
+++ index.php
@@ -9,7 +9,8 @@
 error_reporting(E_ALL);
 ini_set('display_errors', TRUE);
 
-require_once ('vendor/autoload.php');
+require_once('vendor/autoload.php');
+
 
 $f3 = Base::instance();
 
@@ -21,47 +22,5 @@ $f3->route('GET /', function() {
     echo $view->render('views/home.html');
 });
 
-<<<<<<< HEAD
-$f3->route('GET|POST /submission', function($f3) {
-    $view = new View;
-    echo $view->render('models/submission.php');
 
-
-});
-
-=======
-$f3->route('GET|POST /projects', function($f3) {
-    include 'models/database.php';
-    connect();
-    $projects = getProjects();
-    $f3->set('projects', $projects);
-
-    $template = new Template();
-    echo $template->render('views/projects.html');
-});
->>>>>>> 7f4ff090e801042edc5ab35e26396877790f5885
-
-$f3->route('GET|POST /projects/client/@projectID', function($f3, $params) {
-    include 'models/database.php';
-    connect();
-
-    $projectID = $params['projectID'];
-    $clients = getClients($projectID);
-    $f3->set('clients', $clients);
-
-    $template = new Template();
-    echo $template->render('views/clients.html');
-});
-
-$f3->route('GET|POST /developers/@projectID', function($f3, $params) {
-    include 'models/database.php';
-    connect();
-
-    $projectID = $params['projectID'];
-    $developers = getDevelopers($projectID);
-    $f3->set('developers', $developers);
-
-    $template = new Template();
-    echo $template->render('views/developers.html');
-});
 $f3->run();
