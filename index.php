64d06f1ce9f6c3b180524410582ebb442b1b6687
Ashton Knox <aknox3@mail.greenriver.edu>
2018-03-19 16:01:04 -0400

    merge fixing

--- index.php
+++ index.php
@@ -5,28 +5,48 @@
  * Date: 3/6/2018
  * Time: 2:56 PM
  */
-
 error_reporting(E_ALL);
 ini_set('display_errors', TRUE);
-
-require_once('vendor/autoload.php');
-
-
+require_once ('vendor/autoload.php');
 $f3 = Base::instance();
-
 //turn on error reporting
 $f3->set('DEBUG',3);
-
 $f3->route('GET /', function() {
     $view = new View;
     echo $view->render('views/home.html');
 });
 
+
 $f3->route('GET|POST /submission', function($f3) {
     $view = new View;
     echo $view->render('models/submission.php');
-    $f3->reroute("views/submit.html");
 });
 
+$f3->route('GET|POST /projects', function($f3) {
+    include 'models/database.php';
+    connect();
+    $projects = getProjects();
+    $f3->set('projects', $projects);
+    $template = new Template();
+    echo $template->render('views/projects.html');
+});
 
+$f3->route('GET|POST /clients/@projectID', function($f3, $params) {
+    include 'models/database.php';
+    connect();
+    $projectID = $params['projectID'];
+    $clients = getClients($projectID);
+    $f3->set('clients', $clients);
+    $template = new Template();
+    echo $template->render('views/clients.html');
+});
+$f3->route('GET|POST /developers/@projectID', function($f3, $params) {
+    include 'models/database.php';
+    connect();
+    $projectID = $params['projectID'];
+    $developers = getDevelopers($projectID);
+    $f3->set('developers', $developers);
+    $template = new Template();
+    echo $template->render('views/developers.html');
+});
 $f3->run();
--- models/database.php
+++ models/database.php
@@ -12,7 +12,6 @@
     projectURL VARCHAR(100),
     projectLogin VARCHAR(100),
     projectPass VARCHAR(100)
-
     );
  *
 create table clients (
@@ -34,19 +33,15 @@ create table developers (
     FOREIGN KEY(projectID) REFERENCES projects(projectID)
     );
  */
-
 /*
  * Author: Ashton Knox
  * Date: 3/14/2018
  * Database functions
  */
-
 if (file_exists("/home/mtaylorg/config.php"))
     require "/home/mtaylorg/config.php";
 else
     require "/home/aknoxgre/config.php";
-
-
 /**
  * create a connection object
  */
@@ -64,7 +59,6 @@ function connect()
         return;
     }
 }
-
 /**
  * @return array associative array of all projects and their fields
  */
@@ -72,47 +66,34 @@ function getProjects()
 {
     global $dbh;
     $dbh=connect();
-
     //1. Define the query
     $sql = "SELECT * FROM projects";
-
     //2. Prepare the statement
     $statement = $dbh->prepare($sql);
-
     //3. Bind parameters
-
     //4. Execute the query
     $statement->execute();
-
     //5. Get the results
     $result = $statement->fetchAll(PDO::FETCH_ASSOC);
     //print_r($result);
     return $result;
 }
-
 function getProjectID($projectName)
 {
     $dbh=connect();
-
     //1. Define the query
     $sql = "SELECT projectID FROM projects WHERE title=:title";
-
     //2. Prepare the statement
     $statement = $dbh->prepare($sql);
-
     //3. bind params
     $statement->bindParam(':title',$projectName, PDO::PARAM_INT);
-
     //4. Execute the query
     $statement->execute();
-
     //5. Get the results
     $result = $statement->fetch(PDO::FETCH_ASSOC);
     //print_r($result);
-
     return $result['projectID'];
 }
-
 /**
  * @param $title
  * @param $description
@@ -128,15 +109,11 @@ function getProjectID($projectName)
  */
 function insertProject($title, $description, $clientCompany, $clientLocation, $clientSite, $status, $trello, $projectURL, $projectLogin, $projectPass)
 {
-
     $dbh=connect();
-
     //1. Define the query
     $sql = "INSERT INTO projects(title, description, clientCompany, clientLocation, clientSite, status, trello, projectURL, projectLogin, projectPass) VALUES (:title, :description, :clientCompany, :clientLocation, :clientSite, :status, :trello, :projectURL, :projectLogin, :projectPass)";
-
     //2. Prepare the statement
     $statement = $dbh->prepare($sql);
-
     //3. Bind parameters
     $statement->bindParam(':title', $title, PDO::PARAM_STR);
     $statement->bindParam(':description', $description, PDO::PARAM_STR);
@@ -150,35 +127,25 @@ function insertProject($title, $description, $clientCompany, $clientLocation, $c
     $statement->bindParam(':projectPass', $projectPass, PDO::PARAM_STR);
     //4. Execute the query
     $statement->execute();
-
-
 }
-
 /**
  * @return array all clients and their fields
  */
 function getClients($projectID)
 {
-
     $dbh=connect();
-
     //1. Define the query
     $sql = "SELECT * FROM clients WHERE projectID =:projectID";
-
     //2. Prepare the statement
     $statement = $dbh->prepare($sql);
-
     //3. Bind parameters
-
     //4. Execute the query
     $statement->execute();
-
     //5. Get the results
     $result = $statement->fetchAll(PDO::FETCH_ASSOC);
     //print_r($result);
     return $result;
 }
-
 /**
  * @param $clientName
  * @param $clientEmail
@@ -188,26 +155,19 @@ function getClients($projectID)
  */
 function insertClient($clientName, $clientEmail, $clientPhone, $projectID)
 {
-
     $dbh=connect();
-
     //1. Define the query
     $sql = "INSERT INTO clients(clientName, clientEmail, clientPhone, projectID) VALUES (:clientName, :clientEmail, :clientPhone, :projectID)";
-
     //2. Prepare the statement
     $statement = $dbh->prepare($sql);
-
     //3. Bind parameters
     $statement->bindParam(':clientName', $clientName, PDO::PARAM_STR);
     $statement->bindParam(':clientEmail', $clientEmail, PDO::PARAM_STR);
     $statement->bindParam(':clientPhone', $clientPhone, PDO::PARAM_STR);
     $statement->bindParam(':projectID', $projectID, PDO::PARAM_INT);
-
     //4. Execute the query
     $statement->execute();
-
 }
-
 /**
  * @return array all developers and their fields
  */
@@ -215,24 +175,18 @@ function getDevelopers($projectID)
 {
     global $dbh;
     $dbh=connect();
-
     //1. Define the query
     $sql = "SELECT * FROM developers WHERE projectID =:projectID";
-
     //2. Prepare the statement
     $statement = $dbh->prepare($sql);
-
     //3. Bind parameters
-
     //4. Execute the query
     $statement->execute();
-
     //5. Get the results
     $result = $statement->fetchAll(PDO::FETCH_ASSOC);
     //print_r($result);
     return $result;
 }
-
 /**
  * @param $className
  * @param $instructor
@@ -245,23 +199,18 @@ function insertDevelopers($className, $instructor, $quarter, $instructorNotes, $
 {
     global $dbh;
     $dbh=connect();
-
     //1. Define the query
     $sql = "INSERT INTO developers(className, instructor, quarter, instructorNotes, projectID) VALUES (:className, :instructor, :quarter, :instructorNotes, :projectID)";
-
     //2. Prepare the statement
     $statement = $dbh->prepare($sql);
-
     //3. Bind parameters
     $statement->bindParam(':className', $className, PDO::PARAM_STR);
     $statement->bindParam(':instructor', $instructor, PDO::PARAM_STR);
     $statement->bindParam(':quarter', $quarter, PDO::PARAM_STR);
     $statement->bindParam(':instructorNotes', $instructorNotes, PDO::PARAM_STR);
     $statement->bindParam(':projectID', $projectID, PDO::PARAM_STR);
-
     //4. Execute the query
     $result = $statement->execute();
-
     //5. Return the result
     return $result;
 }
